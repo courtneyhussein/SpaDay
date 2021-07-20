45415d0c593dcaef22419d5557565011809c97da
CourtneyHussein <courtneyhussein124@gmail.com>
2021-07-20 13:49:03 -0400

    finished studio

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SpaDay.Controllers
 {
@@ -55,6 +54,12 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+
+            ViewBag.name = name;
+            ViewBag.skintype = skintype;
+            ViewBag.facials = appropriateFacials;
+            ViewBag.manipedi = manipedi;
+
             return View();
         }
 
